5c5f17b13aa8ddf917464e6299225b90d9d27efe
HappyGentai <louis467886@gmail.com>
2023-08-21 15:08:27 +0800

    TestUpload

---
+++ Runtime/Test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace GrazerCore
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hi, this is my custom package test script!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
